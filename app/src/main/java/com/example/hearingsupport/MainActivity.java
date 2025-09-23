d3fb67f1bfb842b6f5757cab1f079ca057a43417
nelea <sceastlivaianelea@gmail.com>
2025-09-23 21:34:11 +0300

    Calendar in progress

--- app/src/main/java/com/example/hearingsupport/MainActivity.java
+++ app/src/main/java/com/example/hearingsupport/MainActivity.java
@@ -36,10 +36,8 @@ private ActivityMainBinding binding;
         });
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_calendar)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
@@ -49,7 +47,6 @@ private ActivityMainBinding binding;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
--- app/src/main/java/com/example/hearingsupport/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/hearingsupport/ui/gallery/GalleryFragment.java
@@ -35,30 +35,9 @@ private FragmentGalleryBinding binding;
             Intent browsIntent = new Intent(Intent.ACTION_VIEW,
                     Uri.parse("https://www.google.com/search?q=" + Uri.encode(query)));
             startActivity(browsIntent);
-//            Intent applicationIntent  = new Intent();
-//            PackageManager packageManager = getPackageManager();
-//            ComponentName componentName = applicationIntent.resolveActivity(packageManager);
-//            if (componentName == null) {
-//                String query = input.getText().toString();
-//                Intent browsIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/search?q=" +
-//                        Uri.encode(query)));
-//                if (browsIntent.resolveActivity(packageManager) != null) {
-//                    startActivity(browsIntent);
-//                } else {
-//                    startActivity(applicationIntent);
-//
-//                }
-//            }
         });
 
         binding.btn2.setOnClickListener(v -> {
-//            if (android.os.Build.VERSION.SDK_INT >= 33) {
-//                if (requireContext().checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS)
-//                        != android.content.pm.PackageManager.PERMISSION_GRANTED) {
-//                    requestPermissions(new String[]{android.Manifest.permission.POST_NOTIFICATIONS}, 100);
-//                    return;
-//                }
-//            }
             long triggerAt = System.currentTimeMillis() + 10_000L;
 
             android.content.Intent i = new android.content.Intent(requireContext(), NotificationReceiver.class);
@@ -84,12 +63,7 @@ private FragmentGalleryBinding binding;
                         "Точная тревога недоступна, использую обычную — может сработать с небольшой задержкой",
                         android.widget.Toast.LENGTH_SHORT).show();
             }
-//                    am.setExactAndAllowWhileIdle(android.app.AlarmManager.RTC_WAKEUP, triggerAt, pi);
-//
-//                    android.widget.Toast.makeText(requireContext(), "Уведомление будет показано через 10 секунд", android.widget.Toast.LENGTH_SHORT).show();
         });
-//        final TextView textView = binding.btn1;
-//        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
